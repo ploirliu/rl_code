247c1631a37d0bf71e1ce0a6a2b38f1a9f16689b
liubo <liubo@xmov.ai>
2021-08-11 16:34:00 +0800

    对代码进行了重构

--- land_question.py
+++ land_question.py
@@ -15,25 +15,38 @@ import os
 
 import time
 
+from abc import abstractmethod
+
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter(os.path.join('logData', time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time()))))
 
 
 def get_env():
     env=gym.make('LunarLander-v2')
     return env
 
-class PolicyGradientNet(nn.Module):
-    def __init__(self):
+class LayerNet(nn.Module):
+    def __init__(self,input_dim,output_dim,dropout):
         super().__init__()
         self.net=nn.Sequential(
-            nn.Linear(8,16),
-            nn.ReLU(),
-            nn.Linear(16,32),
-            nn.ReLU(),
-            nn.Linear(32,16),
-            nn.ReLU(),
-            nn.Linear(16,4)
+            nn.Linear(input_dim,output_dim),
+            nn.ReLU()
+        )
+        self.dropout=dropout
+    def forward(self,input_data):
+        out=self.net(input_data)
+        if self.dropout>0:
+            out=F.dropout(out,self.dropout,training=self.training)
+        return out
+
+class RL_Net(nn.Module):
+    def __init__(self,dropout=0.2):
+        super().__init__()
+        self.net=nn.Sequential(
+            LayerNet(8,32,dropout),
+            LayerNet(32,64,dropout),
+            LayerNet(64,16,dropout),
+            LayerNet(16,64,dropout),
+            nn.Linear(64,4)
         )
     
     def forward(self,state):
@@ -48,28 +61,6 @@ class PolicyGradientNet(nn.Module):
         self.load_state_dict(data)
 
 
-class PolicyGradientAgent():
-    def __init__(self,network) -> None:
-        self.net=network
-        self.optimizer=optim.Adam(self.net.parameters(),lr=1e-3,weight_decay=1e-4)
-        # self.optimizer=optim.Adam(self.net.parameters())
-    
-    def learn(self,log_probs,reward):
-        # loss=(-log_probs*reward).sum()
-        loss=(-log_probs*reward).mean()
-        # loss/=play_num
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
-    def sample(self,state):
-        action_prob=self.net(torch.FloatTensor(state))
-        action_dist=Categorical(action_prob)
-        action=action_dist.sample()
-        log_prob=action_dist.log_prob(action)
-        return action.item(),log_prob
-
-
 def compute_reward(now_reward,alpha):
     now_len=len(now_reward)
     out=[i for i in now_reward]
@@ -77,194 +68,177 @@ def compute_reward(now_reward,alpha):
         out[i-1]+=(alpha*out[i])
     return out
 
-
-def train(dir_path):
-    network=PolicyGradientNet()
-    agent=PolicyGradientAgent(network)
-
-    agent.net.train()
-    EPISODE_PER_BATCH=10
-    NUM_BATCH=100000
-
-    SAVE_BATCH=100
-
-    avg_total_rewards,avg_fin_rewards=[],[]
-    env=get_env()
-    alpha=0.6
-
-    for iter in range(NUM_BATCH):
-        log_probs,rewards=[],[]
-        total_rewards,fin_rewards=[],[]
-
-        for _ in range(EPISODE_PER_BATCH):
-            state=env.reset()
-            total_reward=0
-
-            now_log_probs,now_reward=[],[]
-
-            while True:
-                action,log_prob=agent.sample(state)
-                state,reward,done,_=env.step(action)
-
-                now_log_probs.append(log_prob)
-                now_reward.append(reward)
-
-                total_reward+=reward
-
-                if done:
-                    total_rewards.append(total_reward)
-                    fin_rewards.append(reward)
-
-                    now_reward=compute_reward(now_reward,alpha)
-                    log_probs=log_probs+now_log_probs
-                    rewards=rewards+now_reward
-                    break
-        ave_total_reward=sum(total_rewards)/len(total_rewards)
-        ave_fin_reward=sum(fin_rewards)/len(fin_rewards)
-        avg_total_rewards.append(ave_total_reward)
-        avg_fin_rewards.append(ave_fin_reward)
-
-        if iter%SAVE_BATCH==0:
-            print(f"Total:{ave_total_reward:4.1f},Final:{ave_fin_reward:4.1f}")
-            agent.net.save(os.path.join(dir_path,str(iter)+'.cpt'))
-
-        writer.add_scalar('total',ave_total_reward,iter)
-        writer.add_scalar('final',ave_fin_reward,iter)
-
-        rewards=np.array(rewards)
-        # rewards=(rewards-np.mean(rewards))/(np.std(rewards)+1e-5)
-        rewards=(rewards-np.mean(rewards))
-
-        agent.learn(torch.stack(log_probs),torch.from_numpy(rewards))
-    # return avg_total_rewards,avg_fin_rewards
-
-
-class PPO2_Agent():
-    def __init__(self,network,train_step,e) -> None:
-        self.net=network
-        self.optimizer=optim.Adam(self.net.parameters(),lr=1e-3,weight_decay=1e-4)
-        # self.optimizer=optim.Adam(self.net.parameters())
-        self.train_step=train_step
-        self.e=e
+class RL_Agent():
+    def __init__(self,net,lr=1e-3,weight_decay=0) -> None:
+        self.net=net
+        self.optimizer=optim.Adam(self.net.parameters(),lr=lr,weight_decay=weight_decay)
     
-    def learn(self,state,action,probs,reward):
+    @abstractmethod
+    def compute_loss(self,states,actions,probs,rewards):
+        pass
+
+    @abstractmethod
+    def learn(self,states,actions,probs,rewards):
         self.net.train()
-        for _ in range(self.train_step):
-            state=state.detach()
-            action=action.detach()
-            probs=probs.detach()
-            reward=reward.detach()
+        self.optimizer.zero_grad()
 
+        states=states.detach()
+        actions=actions.detach()
+        rewards=rewards.detach()
+        probs=probs.detach()
 
-            now_action_prob=self.net(state)
+        loss=self.compute_loss(states,actions,probs,rewards)
 
-            now_probs=[now_action_prob[i,action[i,0].int()] for i in range(action.shape[0])]
-            now_probs=torch.stack(now_probs).unsqueeze(-1)
+        loss.backward()
+        self.optimizer.step()
 
-            ori_expectation=now_probs*reward/probs
-            clip_expectation=torch.clip(now_probs/probs,1-self.e,1+self.e)*reward
+    def play(self,env,reward_alpha):
+        self.net.eval()
+        
+        state=env.reset()
 
-            fin_expectation=torch.min(ori_expectation,clip_expectation)
-            loss=(-fin_expectation).sum()
+        now_states=[]
+        now_actions=[]
+        now_probs=[]
+        now_rewards=[]
 
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
+        total_reward=0
+        while True:
+            action_prob=self.net(torch.FloatTensor(state))
+            action_dist=Categorical(action_prob)
+            action=action_dist.sample().item()
+            prob=action_prob[action]
+            
+            now_states.append(state)
+            now_actions.append(action)
+            now_probs.append(prob)
 
-    def sample(self,state):
-        action_prob=self.net(torch.FloatTensor(state))
-        action_dist=Categorical(action_prob)
-        action=action_dist.sample()
-        prob=action_prob[action.item()]
-        return action.item(),prob
+            state,reward,done,_=env.step(action)
+            
+            now_rewards.append(reward)
 
-def PPO2_train(dir_path):
+            total_reward+=reward
+
+            if done:
+                now_reward=compute_reward(now_rewards,reward_alpha)
+                break
+        return now_states,now_actions,now_probs,now_rewards,total_reward,reward
+
+class PolicyGradientAgent(RL_Agent):
+    def __init__(self, net, lr, weight_decay) -> None:
+        super().__init__(net, lr=lr, weight_decay=weight_decay)
+
+    @abstractmethod
+    def compute_loss(self,states,actions,probs,rewards):
+        now_action_prob=self.net(states)
+
+        now_probs=[now_action_prob[i,actions[i,0].int()] for i in range(actions.shape[0])]
+        now_probs=torch.stack(now_probs).unsqueeze(-1)
+
+        now_log_probs=torch.log(now_probs)
+        loss=(-now_log_probs*rewards).mean()
+        return loss
     
-    EPISODE_PER_BATCH=10
+    
+    @abstractmethod
+    def learn(self,states,actions,probs,rewards):
+        super().learn(states,actions,probs,rewards)
+
+class PPO2_Agent(RL_Agent):
+    def __init__(self, net,train_step, lr, weight_decay) -> None:
+        super().__init__(net, lr=lr, weight_decay=weight_decay)
+        self.train_step=train_step
+        self.e=0.4
+    
+    @abstractmethod
+    def compute_loss(self,states,actions,probs,rewards):
+        now_action_prob=self.net(states)
+
+        now_probs=[now_action_prob[i,actions[i,0].int()] for i in range(actions.shape[0])]
+        now_probs=torch.stack(now_probs).unsqueeze(-1)
+
+        ori_expectation=now_probs*rewards/probs
+        clip_expectation=torch.clip(now_probs/probs,1-self.e,1+self.e)*rewards
+
+        fin_expectation=torch.min(ori_expectation,clip_expectation)
+        loss=(-fin_expectation).mean()
+        return loss
+    
+    @abstractmethod
+    def learn(self,states,actions,probs,rewards):
+        for _ in range(self.train_step):
+            super().learn(states,actions,probs,rewards)
+
+def AgentFactory(agent_name,lr,train_step):
+    net=RL_Net()
+
+    weight_decay=0
+
+    if agent_name=="PPO2":
+        out=PPO2_Agent(net,train_step,lr,weight_decay)
+    else:
+        out=PolicyGradientAgent(net,lr,weight_decay)
+    return out
+
+
+def train(out_dir,agent_name):
+    lr=1e-3
+    train_step=20
+
+    PLAY_PER_BATCH=10
     NUM_BATCH=100000
-
     SAVE_BATCH=100
-    alpha=0.6
+    alpha=0.8
 
-    ppo_train_step=3
-    ppo_e=0.2
+    if os.path.exists(out_dir)==False:
+        os.mkdir(out_dir)
 
-    network=PolicyGradientNet()
-    agent=PPO2_Agent(network,ppo_train_step,ppo_e)
+    writer = SummaryWriter(os.path.join('logData', time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time()))+agent_name))
+   
+    agent=AgentFactory(agent_name,lr,train_step)
 
-    avg_total_rewards,avg_fin_rewards=[],[]
+    
+
     env=get_env()
 
     for iter in range(NUM_BATCH):
-        
-        agent.net.eval()
 
-        states,actions,probs,rewards=[],[],[],[]
-        total_rewards,fin_rewards=[],[]
+        all_total_reward=[]
+        all_fin_reward=[]
 
-        for _ in range(EPISODE_PER_BATCH):
-            state=env.reset()
+        all_states,all_actions,all_probs,all_rewards=[],[],[],[]
 
+        for _ in range(PLAY_PER_BATCH):
+            now_states,now_actions,now_probs,now_rewards,total_reward,fin_reward=agent.play(env,alpha)
 
-            total_reward=0
+            all_total_reward.append(total_reward)
+            all_fin_reward.append(fin_reward)
 
-            now_reward=[]
+            all_states+=now_states
+            all_actions+=now_actions
+            all_probs+=now_probs
+            all_rewards+=now_rewards
 
-            while True:
-                action,prob=agent.sample(state)
-
-                states.append(state)
-                actions.append(action)
-
-                state,reward,done,_=env.step(action)
-
-                probs.append(prob)
-                now_reward.append(reward)
-
-                total_reward+=reward
-
-                if done:
-                    total_rewards.append(total_reward)
-                    fin_rewards.append(reward)
-
-                    now_reward=compute_reward(now_reward,alpha)
-                    rewards=rewards+now_reward
-                    break
-        ave_total_reward=sum(total_rewards)/len(total_rewards)
-        ave_fin_reward=sum(fin_rewards)/len(fin_rewards)
-        avg_total_rewards.append(ave_total_reward)
-        avg_fin_rewards.append(ave_fin_reward)
+        ave_total_reward=sum(all_total_reward)/len(all_total_reward)
+        ave_fin_reward=sum(all_fin_reward)/len(all_fin_reward)
 
         if iter%SAVE_BATCH==0:
             print(f"Total:{ave_total_reward:4.1f},Final:{ave_fin_reward:4.1f}")
-            agent.net.save(os.path.join(dir_path,str(iter)+'.cpt'))
+            agent.net.save(os.path.join(out_dir,str(iter)+'.cpt'))
 
         writer.add_scalar('total',ave_total_reward,iter)
         writer.add_scalar('final',ave_fin_reward,iter)
 
-        rewards=np.array(rewards)
-        # rewards=(rewards-np.mean(rewards))/(np.std(rewards)+1e-5)
-        rewards=(rewards-np.mean(rewards))
+        all_rewards=np.array(all_rewards)
+        all_rewards=(all_rewards-np.mean(all_rewards))/(np.std(all_rewards)+1e-5)
 
-        probs=(torch.stack(probs)).unsqueeze(-1)
-        rewards=(torch.from_numpy(rewards)).unsqueeze(-1)
-        states=torch.from_numpy(np.stack(states))
-        actions=(torch.FloatTensor(actions)).unsqueeze(-1)
-        agent.learn(states,actions,probs,rewards)
-    # return avg_total_rewards,avg_fin_rewards
-
+        all_probs=(torch.stack(all_probs)).unsqueeze(-1)
+        all_rewards=(torch.from_numpy(all_rewards)).unsqueeze(-1)
+        all_states=torch.from_numpy(np.stack(all_states))
+        all_actions=(torch.FloatTensor(all_actions)).unsqueeze(-1)
+            
+        agent.learn(all_states,all_actions,all_probs,all_rewards)
 
 if __name__ == "__main__":
-    PPO2_train(r'D:\code\rl_code\ppo2_out')
-    
-    # train(r'D:\code\rl_code\out')
-
-    # avg_total_rewards,avg_fin_rewards=train()
-
-    # plt.plot(avg_total_rewards)
-    # plt.title('total reward')
-    # plt.show()
-
-    # plt.plot(avg_fin_rewards)
-    # plt.title('final reward')
-    # plt.show()
+    train(r'D:\code\rl_code\out','PG')
+    # train(r'D:\code\rl_code\out','PPO2')
